b196dcc670f76341240e2540052aa9c53120bde6
Carla Goldstein <carla.goldstein@gmail.com>
2015-02-18 14:16:53 +0200

    toilet blocks are now added the same as classrooms

--- client/templates/Survey/survey.js
+++ client/templates/Survey/survey.js
@@ -188,7 +188,9 @@ Template.classroom_table.rendered = function(){
 }
 Template.lab_table.rendered = function(){
      $.material.init(); 
-
+}
+Template.block_table.rendered  = function() {
+       $.material.init(); 
 }
 
 AutoForm.hooks({
