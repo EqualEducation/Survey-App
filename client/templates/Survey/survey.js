ab75363d1feefb64217a1ea9411e90d2af9f954b
Carla Goldstein <carla.goldstein@gmail.com>
2015-03-11 10:38:06 +0200

    put back old router and refresh of material

--- client/templates/Survey/survey.js
+++ client/templates/Survey/survey.js
@@ -98,87 +98,87 @@ Template.survey13.events({
 
 //NOTE: We need to re initialize the material css for some reason every time a template is rendered. 
 //TODO: Use one method for all rendered templates instead of having to define each on ehere
-// Template.survey1.rendered = function(){
-//   $.material.init();
-// };
+Template.survey1.rendered = function(){
+  $.material.init();
+};
 
-// Template.survey3.rendered = function(){
+Template.survey3.rendered = function(){
+  $.material.init();
+};
+Template.survey4.rendered = function(){
+  $.material.init();
+};
+Template.survey5.rendered = function(){
+  $.material.init();
+};
+Template.survey6.rendered = function(){
+  $.material.init();
+};
+// Template.survey7.rendered = function(){
 //   $.material.init();
 // };
-// Template.survey4.rendered = function(){
-//   $.material.init();
-// };
-// Template.survey5.rendered = function(){
-//   $.material.init();
-// };
-// Template.survey6.rendered = function(){
-//   $.material.init();
-// };
-// // Template.survey7.rendered = function(){
-// //   $.material.init();
-// // };
-// Template.survey8.rendered = function(){
-//   $.material.init();
-// };
-// Template.survey9.rendered = function(){
-//   $.material.init();
-// };
-// Template.survey10.rendered = function(){
-//   $.material.init();
-// };
-// Template.survey11.rendered = function(){
-//   $.material.init();
-// };
-// Template.survey12.rendered = function(){
-//   $.material.init();
-// };
-// Template.survey13.rendered = function(){
-//   $.material.init();
-// };
-// Template.libraries.rendered = function(){
-//     $.material.init();
-// }
-// Template.security.rendered = function(){
-//       $.material.init();
-// }
-// Template.electricity.rendered = function(){
-//       $.material.init();
-// }
-// Template.sciencelabs.rendered = function(){
-//       $.material.init();
-// }
-// Template.afArrayField.rendered = function(){
+Template.survey8.rendered = function(){
+  $.material.init();
+};
+Template.survey9.rendered = function(){
+  $.material.init();
+};
+Template.survey10.rendered = function(){
+  $.material.init();
+};
+Template.survey11.rendered = function(){
+  $.material.init();
+};
+Template.survey12.rendered = function(){
+  $.material.init();
+};
+Template.survey13.rendered = function(){
+  $.material.init();
+};
+Template.libraries.rendered = function(){
+    $.material.init();
+}
+Template.security.rendered = function(){
+      $.material.init();
+}
+Template.electricity.rendered = function(){
+      $.material.init();
+}
+Template.sciencelabs.rendered = function(){
+      $.material.init();
+}
+Template.afArrayField.rendered = function(){
+   $.material.init(); 
+}
+// Template.classroom_table.rendered = function(){
 //    $.material.init(); 
 // }
-// // Template.classroom_table.rendered = function(){
-// //    $.material.init(); 
-// // }
-// Template.lab_table.rendered = function(){
-//   $.material.init(); 
-// }
-// Template.block_details.rendered  = function() {
-//   $.material.init(); 
-// }
+Template.lab_table.rendered = function(){
+  $.material.init(); 
+}
+Template.block_details.rendered  = function() {
+  $.material.init(); 
+}
 
-// AutoForm.hooks({
-//   survey: {
-//       // onSuccess: function(operation, result, template) {  
-//       //   console.log("Succes result: " + result);
-//       //   console.log("Success operation: " + operation);
-//       //   alert('School has been updated');
-//       // },
-//       onError: function(operation, error, template) {
-//         alert('Could not save the form. Please check all fields are filled in correctly' + error);
+AutoForm.hooks({
+  survey: {
+      // onSuccess: function(operation, result, template) {  
+      //   console.log("Succes result: " + result);
+      //   console.log("Success operation: " + operation);
+      //   alert('School has been updated');
+      // },
+      onError: function(operation, error, template) {
+        alert('Could not save the form. Please check all fields are filled in correctly' + error);
 
-//       },
-//       // onSubmit : function(doc) {
-//       //   console.log("Submit: " + doc);
-//       //   // doc.groupId = /*Get the group id*/;
-//       //   // this.done(); //We've finished
-//       //   return true; //Let autoForm do his default job now
-//       // }
-//     }
-//   });
+      },
+      // onSubmit : function(doc) {
+      //   console.log("Submit: " + doc);
+      //   // doc.groupId = /*Get the group id*/;
+      //   // this.done(); //We've finished
+      //   return true; //Let autoForm do his default job now
+      // }
+    }
+  });
 
 
 
