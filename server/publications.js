44c4b00f06b838e3a98863808cce21f35b028a75
Carla Goldstein <carla.goldstein@gmail.com>
2015-04-08 02:04:56 +0200

    added id filtering to survey sections

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,9 @@
 if (Meteor.isServer) {
 
-  Meteor.publish("schoolnames", function () {
+  Meteor.publish("schoolnames", function (schoolId) {
+    if (schoolId) {
+       return Schools.find({'_id' : schoolId}, {'schoolDetails.INSTITUTION_NAME' : 1}, {sort: {'schoolDetails.INSTITUTION_NAME': 'asc'}});
+    }
     return Schools.find({}, {'schoolDetails.INSTITUTION_NAME' : 1}, {sort: {'schoolDetails.INSTITUTION_NAME': 'asc'}});
   });
 
@@ -8,63 +11,70 @@ if (Meteor.isServer) {
     return Schools.find({},{sort: {'schoolDetails.INSTITUTION_NAME': 1}}, {sort: {'schoolDetails.INSTITUTION_NAME': 'asc'}});
   });
 
-  Meteor.publish("electronicConnectivity", function () {
-    return ElectronicConnectivity.find();
+  Meteor.publish("electronicConnectivity", function (versionId) {
+    return ElectronicConnectivity.find({'version_id' : versionId});
   });
 
 
-  Meteor.publish("contactpeople", function () {
-    return ContactPeople.find();
+  Meteor.publish("contactpeople", function (versionId) {
+    return ContactPeople.find({'version_id' : versionId});
   });
 
-  Meteor.publish("grades", function () {
-    return Grades.find();
+  Meteor.publish("grades", function (versionId) {
+    return Grades.find({'version_id' : versionId});
   });
 
-  Meteor.publish("sports", function () {
-    return Sports.find();
+  Meteor.publish("sports", function (versionId) {
+
+    return Sports.find({'version_id' : versionId});
   });
   
-  Meteor.publish("libraries", function () {
-    return Libraries.find();
+  Meteor.publish("libraries", function (versionId) {
+    return Libraries.find({'version_id' : versionId});
   });
 
-  Meteor.publish("labs", function () {
-    return Labs.find();
+  Meteor.publish("labs", function (versionId) {
+    return Labs.find({'version_id' : versionId});
   });
-  Meteor.publish("individuallabs", function () {
-    return IndividualLabs.find();
+  Meteor.publish("individuallabs", function (versionId) {
+    return IndividualLabs.find({'version_id' : versionId});
   });
-  Meteor.publish("security", function () {
-    return Security.find();
+  Meteor.publish("security", function (versionId) {
+    return Security.find({'version_id' : versionId});
   });
-  Meteor.publish("sanitation", function () {
-    return Sanitation.find();
-  });
-
-  Meteor.publish("sanitationblocks", function () {
-    return SanitationBlocks.find();
+  Meteor.publish("sanitation", function (versionId) {
+    return Sanitation.find({'version_id' : versionId});
   });
 
-  Meteor.publish("additional", function () {
-    return Additional.find();
-  });
-  Meteor.publish("classrooms", function () {
-    return Classrooms.find();
-  });
-  Meteor.publish("nutrition", function () {
-    return Nutrition.find();
-  });
-  Meteor.publish("electricity", function () {
-    return Electricity.find();
-  });
-  Meteor.publish("specialneeds", function () {
-    return SpecialNeeds.find();
+  Meteor.publish("sanitationblocks", function (versionId) {
+    return SanitationBlocks.find({'version_id' : versionId});
   });
 
- Meteor.publish("surveyVersions", function () {
+  Meteor.publish("additional", function (versionId) {
+    return Additional.find({'version_id' : versionId});
+  });
+  Meteor.publish("classrooms", function (versionId) {
+        // check(versionId);
+
+    return Classrooms.find({'version_id' : versionId});
+  });
+  Meteor.publish("nutrition", function (versionId) {
+    return Nutrition.find({'version_id' : versionId});
+  });
+  Meteor.publish("electricity", function (versionId) {
+    return Electricity.find({'version_id' : versionId});
+  });
+  Meteor.publish("specialneeds", function (versionId) {
+
+    return SpecialNeeds.find({'version_id' : versionId});
+  });
+
+ Meteor.publish("surveyVersions", function (versionId) {
+  if(versionId) {
+    return SurveyVersions.find({'_id' : versionId});
+  }
     return SurveyVersions.find();
-  });
+});
 
 //API
 // Accounts.connection = DDP.connect("http://app.equaleducation.org.za");
